73f2813b1f670ef316ef15d35c33b44cbeec7ccd
Nihal1487 <153359713+Nihal1487@users.noreply.github.com>
2024-01-26 18:21:11 +0530

    element add using form of array

--- Frist_React_app_Hello_World/src/index.js
+++ Frist_React_app_Hello_World/src/index.js
@@ -2,4 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
-ReactDOM.render( <h1>Hello World</h1>, document.getElementById('root'))
+ReactDOM.render(
+[ <h1>Hello World</h1>,
+    <h2>Hello Nima</h2>
+], document.getElementById('root'))
