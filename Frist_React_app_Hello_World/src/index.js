84113f0a0406cd6c619d0aa957524ce31b4cd53e
Nihal1487 <153359713+Nihal1487@users.noreply.github.com>
2024-01-26 21:32:26 +0530

    attribute added  in react

--- Frist_React_app_Hello_World/src/index.js
+++ Frist_React_app_Hello_World/src/index.js
@@ -2,15 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // const name = "Nima"
-const fname = "Nihal "
-const lname = "Chaudhary "
-// only use expretionn dont use statement like if else 
+const fname = "Nihal ";
+const lname = "Chaudhary ";
+const img = "https://picsum.photos/200/200"
+const img2 = "https://picsum.photos/250/200"
+const link = "https://www.youtube.com/"
+// only use expretionn dont use statement like if else
 ReactDOM.render(
   <>
- <h1>
-   {`My name is ${fname} ${lname}`}
- </h1>
- <h2>my lucky number is {1485 + 2}</h2> 
+    <h1 contentEditable="true">{`My name is ${fname} ${lname}`}</h1>
+    <h2>my lucky number is {1485 + 2}</h2>
+    <a href={link}>
+    <img src={img} alt="RandomImages" />
+    </a>
+    <img src={img2} alt="RandomImages" />
   </>,
   document.getElementById("root")
 );
