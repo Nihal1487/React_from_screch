c1142e59db3479bb128d10317578dc1704855442
Nihal1487 <153359713+Nihal1487@users.noreply.github.com>
2024-01-26 18:29:34 +0530

    React.Fragment syttetic sugur added  <> </>

--- Frist_React_app_Hello_World/src/index.js
+++ Frist_React_app_Hello_World/src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
 ReactDOM.render(
-[ <h1>Hello World</h1>,
+  <>
+    <h1>Hello World</h1>
     <h2>Hello Nima</h2>
-], document.getElementById('root'))
+  </>,
+  document.getElementById("root")
+);
