c9bdf3dc5b6fba0c562079f4d010cc41d15f1103
Nihal1487 <153359713+Nihal1487@users.noreply.github.com>
2024-01-26 19:10:31 +0530

    js exprectionn added  use with html

--- Frist_React_app_Hello_World/src/index.js
+++ Frist_React_app_Hello_World/src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const name = "Nima"
+// only use expretionn dont use statement like if else 
 ReactDOM.render(
   <>
-    <h1>Hello World</h1>
-    <h2>Hello Nima</h2>
+ <h1>
+  My name is {name}
+ </h1>
+ <h2>my lucky number is {1485 + 2}</h2> 
   </>,
   document.getElementById("root")
 );
