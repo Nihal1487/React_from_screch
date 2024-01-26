33f94e19923dc009db509c80d2596e18f06e478d
Nihal1487 <153359713+Nihal1487@users.noreply.github.com>
2024-01-26 18:59:18 +0530

    challange 1 added

---
+++ 02_challange_1/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <>
+   <h1>Hello World</h1>
+
+    <p>This five is my fav webseries in Nexflix</p>
+ 
+ <ol>
+  <li>Money Heist</li>
+  <li>Berlin</li>
+  <li>Stranger things</li>
+  <li>Planet</li>
+  <li>Breaking Bad</li>
+ </ol>
+
+  </>,
+  document.getElementById("root")
+);
