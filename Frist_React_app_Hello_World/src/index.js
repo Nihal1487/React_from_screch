d9cfc2af63f67a7cc479f9dba595a8981fa0f8f6
Nihal1487 <153359713+Nihal1487@users.noreply.github.com>
2024-01-27 12:26:34 +0530

    inline css and object css added

--- Frist_React_app_Hello_World/src/index.js
+++ Frist_React_app_Hello_World/src/index.js
@@ -7,17 +7,25 @@ const lname = "Chaudhary ";
 const img = "https://picsum.photos/200/200";
 const img2 = "https://picsum.photos/250/200";
 const link = "https://www.youtube.com/";
+
+const heading = {
+  color: "aliceblue",
+  textAlign: "center",
+  margin: "50px",
+  fontFamily: "Patrick Hand , cursive",
+};
 // only use expretionn dont use statement like if else
 ReactDOM.render(
   <>
     <h1
-      className="heading"
+      className="heading" //style={{color: "green", }}
+      style={heading}
     >{`My name is ${fname} ${lname}`}</h1>
-   <div className="img_div">
-    <a href={link}>
-      <img src={img} alt="RandomImages" />
-    </a>
-    <img src={img2} alt="RandomImages" />
+    <div className="img_div">
+      <a href={link}>
+        <img src={img} alt="RandomImages" />
+      </a>
+      <img src={img2} alt="RandomImages" />
     </div>
   </>,
   document.getElementById("root")
