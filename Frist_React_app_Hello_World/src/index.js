d8caffa25a5ee24ff0907fed6b6d113c71586e88
Nihal1487 <153359713+Nihal1487@users.noreply.github.com>
2024-01-27 11:41:05 +0530

    css added

--- Frist_React_app_Hello_World/src/index.js
+++ Frist_React_app_Hello_World/src/index.js
@@ -1,21 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import "./App.css";
 // const name = "Nima"
 const fname = "Nihal ";
 const lname = "Chaudhary ";
-const img = "https://picsum.photos/200/200"
-const img2 = "https://picsum.photos/250/200"
-const link = "https://www.youtube.com/"
+const img = "https://picsum.photos/200/200";
+const img2 = "https://picsum.photos/250/200";
+const link = "https://www.youtube.com/";
 // only use expretionn dont use statement like if else
 ReactDOM.render(
   <>
-    <h1 contentEditable="true">{`My name is ${fname} ${lname}`}</h1>
-    <h2>my lucky number is {1485 + 2}</h2>
+    <h1
+      className="heading"
+    >{`My name is ${fname} ${lname}`}</h1>
+   <div className="img_div">
     <a href={link}>
-    <img src={img} alt="RandomImages" />
+      <img src={img} alt="RandomImages" />
     </a>
     <img src={img2} alt="RandomImages" />
+    </div>
   </>,
   document.getElementById("root")
 );
