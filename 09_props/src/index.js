7f463bbb2c839b4c6fc639c19e7b8f565c461396
Nihal1487 <153359713+Nihal1487@users.noreply.github.com>
2024-01-29 15:57:48 +0530

    map method added and some css added

--- 09_props/src/index.js
+++ 09_props/src/index.js
@@ -1,30 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Card from "./Card";
-import Sdata from "./Sdata"
-import "./App.css"
+import Sdata from "./Sdata";
+import "./App.css";
+
+// const ncard = (val) => {
+//   return (
+//     <Card
+//       imgsrc={val.imgsrc}
+//       title={val.title}
+//       sname={val.sname}
+//       link={val.link}
+//     />
+//   );
+// }
+
 ReactDOM.render(
   <>
     <h1>List of My Fav Top 5 Nettflix series</h1>
 
-    <Card
-      imgsrc={Sdata[0].imgsrc}
-      title={Sdata[0].title}
-      sname={Sdata[0].sname}
-      link={Sdata[0].link}
-    />
-    <Card
-      imgsrc={Sdata[1].imgsrc}
-      title={Sdata[1].title}
-      sname={Sdata[1].sname}
-      link={Sdata[1].link}
-    />
-    <Card
-      imgsrc={Sdata[2].imgsrc}
-      title={Sdata[2].title}
-      sname={Sdata[2].sname}
-      link={Sdata[2].link}
-    />
+    {Sdata.map((val) => {
+      return (
+        <Card
+          imgsrc={val.imgsrc}
+          title={val.title}
+          sname={val.sname}
+          link={val.link}
+        />
+      );
+    })}
   </>,
   document.getElementById("root")
 );
