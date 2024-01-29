786f1004e623ab84ecbcac99ae4dbb067c63eb87
Nihal1487 <153359713+Nihal1487@users.noreply.github.com>
2024-01-29 15:29:20 +0530

    Array added and code is mopre simple

---
+++ 09_props/src/Sdata.jsx
@@ -0,0 +1,25 @@
+const Sdata = [
+  {
+    imgsrc:
+      "https://wallpapers.com/images/high/money-heist-professor-dali-mask-v0frorkujxu6znlv.webp",
+    title: "A Netflix Orignal series",
+    sname: "Money Heist",
+    link: "https://www.netflix.com/in/title/80192098",
+  },
+  {
+    imgsrc:
+      "https://rukminim2.flixcart.com/image/832/832/jr3t5e80/poster/a/w/f/medium-stranger-things-poster-for-room-office-13-inch-x-19-inch-original-imafcww8v99r4qhh.jpeg?q=70&crop=false",
+    title: "A Netflix Orignal series",
+    sname: "Stranger Things",
+    link: "https://www.netflix.com/in/title/80057281",
+  },
+  {
+    imgsrc:
+      "https://ik.imagekit.io/8knuijgnsxy/whatcha/movie/6df3149a-d66c-43dc-9341-1aa0828df47f/47d99317-3b2b-4fbf-857c-afa937c9b8e9_lnhbffUyZ.jpg?tr=h-500",
+    title: "A Amazonn prime Orignal series",
+    sname: "Mr.Robot",
+    link: "https://www.netflix.com/in/title/80057281",
+  },
+];
+
+export default Sdata;
--- 09_props/src/index.js
+++ 09_props/src/index.js
@@ -1,27 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Card from "./Card"
-
+import Card from "./Card";
+import Sdata from "./Sdata"
+import "./App.css"
 ReactDOM.render(
   <>
+    <h1>List of My Fav Top 5 Nettflix series</h1>
+
     <Card
-      imgsrc="https://wallpapers.com/images/high/money-heist-professor-dali-mask-v0frorkujxu6znlv.webp"
-      title="A Netflix Orignal series"
-      sname="Money Heist"
-      link="https://www.netflix.com/in/title/80192098"
+      imgsrc={Sdata[0].imgsrc}
+      title={Sdata[0].title}
+      sname={Sdata[0].sname}
+      link={Sdata[0].link}
     />
-    <Card 
-      imgsrc="https://rukminim2.flixcart.com/image/832/832/jr3t5e80/poster/a/w/f/medium-stranger-things-poster-for-room-office-13-inch-x-19-inch-original-imafcww8v99r4qhh.jpeg?q=70&crop=false"
-      title="A Netflix Orignal series"
-      sname="Stranger Things"
-      link="https://www.netflix.com/in/title/80057281"
-      />
-      <Card 
-      imgsrc="https://ik.imagekit.io/8knuijgnsxy/whatcha/movie/6df3149a-d66c-43dc-9341-1aa0828df47f/47d99317-3b2b-4fbf-857c-afa937c9b8e9_lnhbffUyZ.jpg?tr=h-500"
-      title="A Amazonn prime Orignal series"
-      sname="Mr.Robot"
-      link="https://www.netflix.com/in/title/80057281"
-      />
+    <Card
+      imgsrc={Sdata[1].imgsrc}
+      title={Sdata[1].title}
+      sname={Sdata[1].sname}
+      link={Sdata[1].link}
+    />
+    <Card
+      imgsrc={Sdata[2].imgsrc}
+      title={Sdata[2].title}
+      sname={Sdata[2].sname}
+      link={Sdata[2].link}
+    />
   </>,
   document.getElementById("root")
 );
