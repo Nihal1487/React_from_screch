d181a709eb89751922191e6c63549dea96388686
Nihal1487 <153359713+Nihal1487@users.noreply.github.com>
2024-01-30 12:22:38 +0530

    bug fixied and strictt mode added for no bugs

--- 09_props/src/App.js
+++ 09_props/src/App.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
+import Sdata from "./Sdata";
+import "./App.css";
 
 function App() {
   return (
     <>
-   
+      <h1 style={{ textAlign: "center" }}>
+        List of My Fav Top 5 Nettflix series
+      </h1>
+
+      {Sdata.map((val) => {
+        return (
+          <Card
+            key={val.id}
+            imgsrc={val.imgsrc}
+            title={val.title}
+            sname={val.sname}
+            link={val.link}
+          />
+        );
+      })}
     </>
-  )
+  );
 }
 
-
-export default App
+export default App;
--- 09_props/src/index.js
+++ 09_props/src/index.js
@@ -1,34 +1,11 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import Card from "./Card";
-import Sdata from "./Sdata";
-import "./App.css";
-
-// const ncard = (val) => {
-//   return (
-//     <Card
-//       imgsrc={val.imgsrc}
-//       title={val.title}
-//       sname={val.sname}
-//       link={val.link}
-//     />
-//   );
-// }
+import App from "./App";
 
 ReactDOM.render(
-  <>
-    <h1>List of My Fav Top 5 Nettflix series</h1>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 
-    {Sdata.map((val) => {
-      return (
-        <Card
-          imgsrc={val.imgsrc}
-          title={val.title}
-          sname={val.sname}
-          link={val.link}
-        />
-      );
-    })}
-  </>,
   document.getElementById("root")
 );
