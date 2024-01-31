9d042f516c2432481c638147673471487e32ab73
Nihal1487 <153359713+Nihal1487@users.noreply.github.com>
2024-01-31 17:47:57 +0530

    useState added

---
+++ 12_usestate/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const state = useState();
+
+  const [count, setCount] = useState(1);
+
+  const IncNum = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={IncNum}>click me</button>
+    </>
+  );
+};
+
+export default App;
